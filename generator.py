c60f18b17d65d054c49e02d03ae32751d4609ca9
WindLeaf233 <mc.windleaf@foxmail.com>
2022-10-28 20:55:14 +0800

    更新高二期末考试答案

--- generator.py
+++ generator.py
@@ -8,7 +8,6 @@ import json
 import re
 
 
-lib_name_list: List[str] = ['primary', 'middle']
 json_file_name = 'lib2class.json'
 
 def log(*message: Any, color: Union[str, None] = None) -> None:
@@ -28,30 +27,33 @@ def parse_int(string: str) -> int:
 def main(year: int) -> None:
     path: str = f'./{year}'
     result: Dict[str, List[Dict[str, str]]] = {}
-    for name in lib_name_list:
+    files: List[str] = os.listdir(str(year))
+    for filename in files:
+        name: str = filename.strip('.docx')
         file_path: str = f'{path}/{name}.docx'
-        if os.path.exists(file_path): 
-            file: BufferedReader = open(file_path, 'rb')
-            document: Document = Document(file)
-            log(f'成功读取 `{file_path}` 到文档对象: {document}')
-            temp: List[Dict[str, str]] = []
-            for para in document.paragraphs:
-                text: str = para.text.strip()
-                num: re.Match = re.match(r'^[0-9]*(、)', text)
-                ans: re.Match = re.match(r'( *)(答案：)[A-Z]*', text)
-                if para.style.name == 'Normal':
-                    if num:
-                        number: str = num.group()
-                        removed: str = text.replace(number, '')
-                        temp.append({ 'question': removed })
-                    elif ans:
-                        answer: str = ans.group().split('：')[-1]
-                        temp[-1]['answer'] = answer
-            result[name] = temp
-            log(f'({name}) 成功加载 {temp.__len__()} 个题目!', color='green')
-        else:
-            handle_error(f'无法找到 `{file_path}`!')
-            exit(-1)
+        if not name.startswith('~'): # ignore temp file
+            if os.path.exists(file_path):
+                file: BufferedReader = open(file_path, 'rb')
+                document: Document = Document(file)
+                log(f'成功读取 `{file_path}` 到文档对象: {document}')
+                temp: List[Dict[str, str]] = []
+                for para in document.paragraphs:
+                    text: str = para.text.strip()
+                    num: re.Match = re.match(r'^[0-9]*(、)', text)
+                    ans: re.Match = re.match(r'( *)((正确答案|答案)：)[A-Z]*', text)
+                    if 'Normal' in para.style.name:
+                        if num:
+                            number: str = num.group()
+                            removed: str = text.replace(number, '')
+                            temp.append({ 'question': removed })
+                        elif ans:
+                            answer: str = ans.group().split('：')[-1]
+                            temp[-1]['answer'] = answer
+                result[name] = temp
+                log(f'({name}) 成功加载 {temp.__len__()} 个题目!', color='green')
+            else:
+                handle_error(f'无法找到 `{file_path}`!')
+                exit(-1)
     json_file: BufferedWriter = open(json_file_name, 'w', encoding='utf-8')
     json.dump(result, json_file, ensure_ascii=False, indent=2)
     log(f'已写入文件 `{json_file_name}`!')
